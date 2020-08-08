b4a13722c75cd4b788e46fb5b85c3c907348c91a
Tomas Zeman <zemasoft@seznam.cz>
2020-08-08 13:57:40 +0200

    Refactor

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,26 +5,26 @@ set -e
 SCRIPT_BASE=$(basename $(readlink -f $0))
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
-${SCRIPT_DIR}/scripts/whereami.sh
+${SCRIPT_DIR}/scripts/bootstrap/whereami.sh
 
 if [[ ! -v ${USE_FREEALUT_FOR_AUDIO} || ${USE_FREEALUT_FOR_AUDIO} == ON ]]
 then
-  ${SCRIPT_DIR}/scripts/freealut.sh
+  ${SCRIPT_DIR}/scripts/bootstrap/freealut.sh
 fi
 
 if [[ ! -v ${USE_FREEGLUT} || ${USE_FREEGLUT} == ON || ${USE_FREEGLUT_FOR_TEXT} == ON ]]
 then
-  ${SCRIPT_DIR}/scripts/freeglut.sh
+  ${SCRIPT_DIR}/scripts/bootstrap/freeglut.sh
 fi
 
 if [[ ! -v ${USE_GLFW} || ${USE_GLFW} == ON ]]
 then
-  ${SCRIPT_DIR}/scripts/glfw.sh
+  ${SCRIPT_DIR}/scripts/bootstrap/glfw.sh
 fi
 
 if [[ ! -v ${USE_SDL2} || ${USE_SDL2} == ON || ${USE_SDL2_FOR_AUDIO} == ON ]]
 then
-  ${SCRIPT_DIR}/scripts/sdl2.sh
+  ${SCRIPT_DIR}/scripts/bootstrap/sdl2.sh
 fi
 
 echo "${SCRIPT_BASE}: All OK"
